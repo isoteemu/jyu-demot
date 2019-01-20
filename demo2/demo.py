ce25f080403bf20c8f5adc3c2c4d8ce869ee995b
Teemu Autto <teemu@rautakuu.org>
2019-01-20 19:36:08 +0200

    Python korjaukset

--- demo2/demo.py
+++ demo2/demo.py
@@ -12,10 +12,11 @@ class Demo2:
     PIN_PAINIKE = 19
     PIN_PIR = 24
 
-    PIN_JALAKNKULKIJA_VIHREÄ = 12
     PIN_AUTO_PUNAINEN = 16
     PIN_AUTO_KELTAINEN = 20
-    PIN_AUTO_VIHREÄ = 21
+    PIN_AUTO_VIHREA = 21
+    PIN_JALANKULKIJA_VIHREA = 12
+
 
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
@@ -46,56 +47,72 @@ class Demo2:
         GPIO.setup(self.PIN_LED, GPIO.OUT)
         GPIO.setup(in_pin, GPIO.IN)
         tila = GPIO.input(in_pin)
+        print("Sensorin tila: %s" % tila)
         while(True):
             if tila != GPIO.input(in_pin):
                 tila = not tila
-                print("Uusi painikkeen tila: %s" % tila)
+                print("Sensorin tila: %s" % tila)
 
             GPIO.output(self.PIN_LED, tila)
             time.sleep(0.1)
 
-    def liikennevalot():
-        GPIO.setup(self.PIN_JALANKULKIJA_VIHREÄ, GPIO.OUT)
+    def liikennevalot(self):
+        GPIO.setup(self.PIN_JALANKULKIJA_VIHREA, GPIO.OUT)
         GPIO.setup(self.PIN_AUTO_PUNAINEN, GPIO.OUT)
         GPIO.setup(self.PIN_AUTO_KELTAINEN, GPIO.OUT)
-        GPIO.setup(self.PIN_AUTO_VIHREÄ, GPIO.OUT)
+        GPIO.setup(self.PIN_AUTO_VIHREA, GPIO.OUT)
         GPIO.setup(self.PIN_PAINIKE, GPIO.IN)
+        GPIO.setup(self.PIN_PIR, GPIO.IN)
 
-        GPIO.output(self.PIN_JALAKNKULKIJA_VIHREÄ, 0)
+        GPIO.output(self.PIN_JALANKULKIJA_VIHREA, 0)
         GPIO.output(self.PIN_AUTO_PUNAINEN, 0)
-        GPIO.output(self.PIN_AUTO_KELTAINE, 0)
-        GPIO.output(self.PIN_AUTO_VIHREÄ, 1)
+        GPIO.output(self.PIN_AUTO_KELTAINEN, 0)
+        GPIO.output(self.PIN_AUTO_VIHREA, 1)
 
-        while(true):
+        while(True):
 
             # Kun valot ovat vihreät autoilijoille ja painiketta on painettu, 
             # sytytetään signaalivalo (keltainen), joka kertoo jalankulkijalle, 
             # että painallus on rekisteröity. Valon tulee palaa napin
             # painamisesta siihen asti, kunnes jalankulkijoille vaihtuu vihreä valo.
             if GPIO.input(self.PIN_PAINIKE):
-
-                GPIO.output(PIN_AUTO_KELTAINEN, 1)
+                print("Painiketta painettu.")
 
                 if GPIO.input(self.PIN_PIR):
+                    print("Liikkeentunnistimessa liikettä")
                     # Kun nappia painetaan, tarkistetaan, näkyykö liikettä.
                     # Jos ei näy liikettä, ei ole autoja, joten valot voi vaihtaa
                     # jalankulkijoille suotuisiksi. Jos liikettä, odota hetki, jos
                     # jono loppuisi. 
+                    print("Liikkeentunnistimessa liikettä")
                     time.sleep(2)
 
-                GPIO.output(PIN_AUTO_KELTAINEN, 0)
-                GPIO.output(PIN_AUTO_PUNAINEN, 1)
-                GPIO.output(PIN_AUTO_VIHREÄ, 0)
+                GPIO.output(self.PIN_AUTO_KELTAINEN, 1)
+                GPIO.output(self.PIN_AUTO_VIHREA, 0)
+                time.sleep(1)
+
+
+                print("Päästetään jalankulkijat")
+                GPIO.output(self.PIN_AUTO_KELTAINEN, 0)
+                GPIO.output(self.PIN_AUTO_PUNAINEN, 1)
+                GPIO.output(self.PIN_AUTO_VIHREA, 0)
+                GPIO.output(self.PIN_JALANKULKIJA_VIHREA, 1)
 
                 time.sleep(3)
+                
+                print("Päästetään audit")
 
-                GPIO.output(PIN_AUTO_KELTAINEN, 0)
-                GPIO.output(PIN_AUTO_PUNAINEN, 0)
-                GPIO.output(PIN_AUTO_VIHREÄ, 1)
+                GPIO.output(self.PIN_AUTO_KELTAINEN, 1)
+                GPIO.output(self.PIN_JALANKULKIJA_VIHREA, 0)
+
+                time.sleep(1)
+
+                GPIO.output(self.PIN_AUTO_KELTAINEN, 0)
+                GPIO.output(self.PIN_AUTO_PUNAINEN, 0)
+                GPIO.output(self.PIN_AUTO_VIHREA, 1)
 
             time.sleep(0.1)
             
-
 
     def __del__(self):
         GPIO.output(self.PIN_LED, 0)
