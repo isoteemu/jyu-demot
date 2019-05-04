7a86c756e1b2d57669f455fc919f787a338a54d0
Teemu Autto <teemu@rautakuu.org>
2019-05-04 20:04:44 +0300

    Story mode for articles; Shows article in modal carousel

--- tiea2080/vt7/tiea2080/static/app.js
+++ tiea2080/vt7/tiea2080/static/app.js
@@ -1,3 +1,26 @@
+function since(datetime) {
+    var since_values = {
+        "m": 60,
+        "h": 60*60,
+        "d": 60*60*24,
+        "m": 60*60*24*30.4,
+        "y": 60*60*24*30*12+5,
+    };
+
+    var lapsed = (Date.now() - datetime.valueOf()) / 1000;
+
+    var r = lapsed+"s";
+    for(var k in since_values) {
+
+        m = since_values[k];
+        d = lapsed / m;
+        if( d > 1 )
+            r = Math.round(d) + k;
+    }
+    return r;
+
+}
+
 $(document).ready(function() {
     $('#nav-notification').popover({
         content: $('#_notifications').html(),
@@ -16,5 +39,69 @@ $(document).ready(function() {
     // Make popovers dismissable.
     $('.popover-dismiss').popover({
         trigger: 'focus'
+    });
+
+    $('#article-modal').on('show.bs.modal', function (event) {
+        var start_slide = 0;
+        var $article = $(event.relatedTarget).parents(".article");
+        var $feed = $(".feed [name=feed][value=\""+$article.find("[data-feed]").data("feed")+"\"]");
+        var $articles = $article.parents(".articles").find(".article");
+        var $modal = $(this);
+        var $header = $modal.find("header");
+
+        console.log($article.find("data-feed"), $feed);
+        $header.find(".feed").empty().append($feed.html());
+        $header.find("#modal-title-title").attr("href", $article.find("a").attr("href")).empty().text($article.find(".title").text());
+
+        published = new Date($article.find("[data-published]").data("published"));
+        $header.find("#modal-title-published").attr("datetime", published.toISOString()).empty().text(since(published));
+
+        // BODY
+        var $indicators = $modal.find(".carousel-indicators");
+        var $carousel = $modal.find(".carousel-inner");
+
+        var $indicator_tmpl = $modal.find("template#carousel-indicator").html().trim();
+        var $item_tmpl = $modal.find("template#carousel-item").html().trim();
+
+        $indicators.empty();
+        $carousel.empty();
+
+        for(var i=0; i < $articles.length; i++) {
+            var $article_i = $($articles[i]);
+
+            var $large_url = $article_i.find("img[data-large]").data("large");
+            if(!$large_url) continue;
+
+            var $slide = $($item_tmpl);
+            var $indicator = $($indicator_tmpl);
+
+            $indicator.attr("data-slide-to", i);
+
+            $slide.find("img").attr("src", $large_url);
+            $slide.find(".carousel-caption").html(
+                DOMPurify.sanitize($article_i.find(".content").html(), {
+                    ALLOWED_TAGS: ['b', 'strong', 'p', 'a', 'br']
+                })
+            );
+
+            $slide.attr("title", $article_i.find(".title").text())
+            $slide.attr("href", $article_i.find("a").attr("href"));
+
+            if($article_i[0] == $article[0]) {
+                start_slide = i;
+                $slide.addClass("active");
+                $indicator.addClass("active");
+            }
+
+            $indicators.append($indicator);
+            $carousel.append($slide);
+        }
+
+        $carousel.parents(".carousel").carousel(start_slide).on('slide.bs.carousel', function(e) {
+            // Update title
+            $header.find("#modal-title-title").attr("href", $(e.relatedTarget).attr("href"))
+                .empty().text($(e.relatedTarget).attr("title"));
+        });
+
     });
 });
