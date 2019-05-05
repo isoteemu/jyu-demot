cf558cbf479e3e51b3f51d4b938fa8500792dc82
Teemu Autto <teemu@rautakuu.org>
2019-05-05 10:57:57 +0300

    Check for admin instance

--- tiea2080/vt7/admin.py
+++ tiea2080/vt7/admin.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
 
-import os
 from main import app, setup
 from tiea2080 import admin
 
-admin.init_app(app)
-setup(app)
+if __name__ == "admin":
+    admin.init_app(app)
+    setup(app)
